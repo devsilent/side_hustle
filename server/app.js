f683c857307638c912f751ff935704bb719262c2
abc <motocapstone2021@gmail.com>
2021-06-13 19:21:17 +0800

    Added jobs router

--- server/app.js
+++ server/app.js
@@ -2,9 +2,10 @@ const express = require('express')
 const app = express()
 const Port = 3000
 
-app.get('/', (req, res) => {
-    res.send("HELLO")
-})
+// Routers
+const jobs = require('./routers/Jobs')
+
+app.use('/jobs', jobs)
 
 app.listen(Port, ()=> {
     console.log(`App is listening to PORT ${Port}`)
